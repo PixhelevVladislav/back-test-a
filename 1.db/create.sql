c08ac26b052fb7afe4c813a5debc1c81538cbe58
PixhelevVladislav <pixhel1995@gmail.com>
2022-06-17 17:06:21 +0300

    Update create.sql

--- 1.db/create.sql
+++ 1.db/create.sql
@@ -1,1 +1,9 @@
-USE amzmanager
+create table product_passport
+(
+    id           int unsigned auto_increment comment 'ID' primary key,
+    name         varchar(128)           null comment 'Product Name',
+    us_tier_list varchar(32)            null comment 'US TIER LIST',
+    asins        varchar(16) default '' not null comment 'Asins',
+    short_names  varchar(16) default '' not null comment 'Short Names'
+)
+    comment 'UEP Product Passport' charset = utf8;
